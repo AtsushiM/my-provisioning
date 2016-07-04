5483e995d0d294add5d6dfe7812db085f9497d68
Atsushi Mizoue <atsushi_mizoue@qteras.co.jp>
2016-07-04 11:32:37 +0900

    first commit

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+# xcode
+sudo xcodebuild -license
+# homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+# ansible
+brew install ansible
+
+# git
+brew install git
+
